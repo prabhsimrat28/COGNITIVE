c917a4a5941e9893c88ee3aab9fa020d983650fe
prabhsimrat28 <155677348+prabhsimrat28@users.noreply.github.com>
2025-01-18 16:41:54 +0530

    Update Q11.py

---
+++ ASSIGNMENT1/Q11.py
@@ -0,0 +1,6 @@
+import math
+import datetime
+import os
+print("Printitng square root of 100",math.sqrt(100))
+print("Current date",datetime.datetime.now())
+print("Current directory all files",os.listdir(os.getcwd()))
